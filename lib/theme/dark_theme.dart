472db61a276500014cadaa091ec75f7507c2296e
Your Name <you@example.com>
2025-07-09 11:50:47 +0300

    feat: update theme colors and improve Arabic translation for order status
    - Changed primary and secondary colors in both light and dark themes to reflect new branding (coral/pinkish tones)
    - Adjusted `textButtonTheme` and `colorScheme` colors accordingly
    - Updated Arabic translation of "processing" from "يعالج" to "تتم المعالجه" for better clarity

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 ThemeData dark = ThemeData(
   fontFamily: 'Roboto',
-  primaryColor: const Color(0xFF54b46b),
-  secondaryHeaderColor: const Color(0xFF009f67),
+  primaryColor: const Color(0xFFf45f6a),
+  secondaryHeaderColor: const Color.fromARGB(255, 235, 101, 110),
   disabledColor: const Color(0xFF6f7275),
   brightness: Brightness.dark,
   hintColor: const Color(0xFFbebebe),
   cardColor: Colors.black,
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: const Color(0xFF54b46b))),
-  colorScheme: const ColorScheme.dark(primary: Color(0xFF54b46b), secondary: Color(0xFF54b46b)).copyWith(error: const Color(0xFFdd3135)),
+  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: Color(0xFFf45f6a))),
+  colorScheme: const ColorScheme.dark(primary: Color(0xFFf45f6a), secondary: Color(0xFF333e4f)).copyWith(error: const Color(0xFFdd3135)),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white10),
   floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
